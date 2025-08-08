37f2c199963f63ba5c3b6b8861683677ebdd2eba
SunnyKapur <kumarsunnykumar678@gmail.com>
2025-08-08 09:16:51 +0530

    comment modify

--- src/service/ai.service.js
+++ src/service/ai.service.js
@@ -2,7 +2,7 @@ const { GoogleGenAI } = require ("@google/genai");
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({});
-// api key used in .env
+// api key used in .env file not here
 
 async function main() {
   const response = await ai.models.generateContent({
